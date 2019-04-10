d3134fd1b9ff8a9fd5f017afed768d5ec448c743
wxr031 <wxr031@protonmail.com>
2019-04-10 13:26:42 +0800

    scratch

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
 int main() {
-	
+
 	/* innformation of processes*/
 	process_t processes[MAX_PROCESS_NUM];
 	
@@ -16,7 +17,7 @@ int main() {
 	 *    3. SJF (Fhortest Job First)
 	 *    4. PSJF (Preemptive Shortest Job First)
 	 */
-	char priority_type[8];
+	char priority_type[8] = {0};
 
 	/* number of tasks */
 	int task_num;
@@ -29,6 +30,57 @@ int main() {
 	}
 	for (int i = 0; i < task_num; i++) {
 		get_process_data(&processes[i]);
+		processes[i].id = i;
 	}
+
+	int process_order_len = 0;
+	int *process_order = NULL;
+
+	/* decide which scheduling algorithm to use */
+	if (!strcmp(priority_type, "FIFO")) {
+		process_order = /* TODO */
+	}
+	else if (!strcmp(priority_type, "RR")) {
+		process_order = /* TODO */
+	}
+	else if (!strcmp(priority_type, "SJF")) {
+		process_order = /* TODO */
+	}
+	else if (!strcmp(priority_type, "PSJF")) {
+		process_order = /* TODO */
+	}
+	else {
+		die("Invalid priority type %s", prioity_type);
+	}
+
+	pid_t pids[MAX_PROCESS_NUM] = {0};
+	struct sigaction act[MAX_PROCESS_NUM] = {0};
+
+	for (int i = 0; i < task_num; i++) {
+		act[i].sa_handler = sigusr1_handler;
+		act[i].sa_flags = 0;
+		sigfillset(&act[i].sa_mask); /* Block every signal during the handler */
+		sigaction(SIGUSR1, &act[i], NULL);
+	}
+
+	/* simulate process scheduling */
+	int next_process_id;
+	for (int i = 0; i < process_order_len; i++) {
+		next_process_id = process_order[i];
+		if (pids[next_process_id] == 0) {
+			pids[next_process_id] = fork();
+			if (pids[next_process_id] == -1) {
+				die("Error forking child");
+			}
+			else if (pids[next_process_id] == 0) { /* child */
+				sigsuspend(&act[i].sa_mask);
+			}
+		}
+		kill(pids[next_process_id], SIGUSR1);
+		if () { /* if the process terminates */
+			kill(pids[next_process_id], SIGUSR2);
+		}
+	}
+	
 	return 0;
 }
